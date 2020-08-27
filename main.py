51221bff1a64981027fcf1f143111212c3ce1c08
Kamil Rafałko <kamil.rafalko@softwaremill.com>
2020-08-27 06:40:01 +0200

    Add test notebook

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-def nms(boxes, scores):
+import tensorflow as tf
+
+def nms(boxes, scores, proposal_count, nms_threshold):
     print(boxes)
     print(scores)
+
     indices = tf.image.non_max_suppression(
-        boxes, scores, self.proposal_count,
-        self.nms_threshold, name="rpn_non_max_suppression")
+        boxes, scores, proposal_count,
+        nms_threshold, name="rpn_non_max_suppression")
 
     all_boxes = tf.identity(boxes)
     all_scores = tf.identity(scores)
@@ -14,20 +17,23 @@ def nms(boxes, scores):
         candidate = sorted_args[0, :]
 
         def check_box(box, boxes, scores):
-            zA = tf.math.maximum(candidate[0], box[0])
-            yA = tf.math.maximum(candidate[1], box[1])
-            xA = tf.math.maximum(candidate[2], box[2])
-            zB = tf.math.maximum(candidate[3], box[3])
-            yB = tf.math.maximum(candidate[4], box[4])
-            xB = tf.math.maximum(candidate[5], box[5])
+            def iou(a, b):
+                zA = tf.math.maximum(a[0], b[0])
+                yA = tf.math.maximum(a[1], b[1])
+                xA = tf.math.maximum(a[2], b[2])
+                zB = tf.math.maximum(a[3], b[3])
+                yB = tf.math.maximum(a[4], b[4])
+                xB = tf.math.maximum(a[5], b[5])
 
-            interArea = tf.math.maximum(0, xB - xA) * tf.math.maximum(0, yB - yA) * tf.math.maximum(0, zB - zA)
+                interArea = tf.math.maximum(0, xB - xA) * tf.math.maximum(0, yB - yA) * tf.math.maximum(0, zB - zA)
 
-            boxAArea = (candidate[3] - candidate[0]) * (candidate[4] - candidate[1]) * (candidate[5] - candidate[2])
-            boxBArea = (box[3] - box[0]) * (box[4] - box[1]) * (box[5] - box[2])
+                boxAArea = (a[3] - a[0]) * (a[4] - a[1]) * (a[5] - a[2])
+                boxBArea = (b[3] - b[0]) * (b[4] - b[1]) * (b[5] - b[2])
 
-            iou = interArea / float(boxAArea + boxBArea - interArea)
-            if iou > self.nms_threshold:
+                return interArea / (boxAArea + boxBArea - interArea)
+
+            iou = iou(candidate, box)
+            if iou > nms_threshold:
                 boxes = boxes[1:, :]
 
         tf.map_fn(lambda box: check_box(box, boxes, scores), boxes)
